336407949744ba10c5089f7cd7cc04fbf2af1bc6
Joao Sedoc <joao@cis.upenn.edu>
2019-03-14 13:04:42 -0400

    Moved lots of stuff around. Time to clean up ...

--- lists/load_word_lists.py
+++ lists/load_word_lists.py
@@ -7,7 +7,7 @@ if not 'sys_lists' in dir():
     sys_lists = dir()
 sys_lists += 'sys_lists'
 
-wikiWordsPath = '/content/' + 'enwiki-20150602-words-frequency.txt'
+wikiWordsPath = './' + 'enwiki-20150602-words-frequency.txt'
 wikiWords = []
 with open(wikiWordsPath, "r+") as f_in:
     for line in f_in:
@@ -15,7 +15,7 @@ with open(wikiWordsPath, "r+") as f_in:
       if int(one_line[1]) > 200:
         wikiWords.append(one_line[0]) 
         
-wikiWordsPath = '/content/' + '/SIF/auxiliary_data/enwiki_vocab_min200.txt'
+wikiWordsPath = './' + '/SIF/auxiliary_data/enwiki_vocab_min200.txt'
 with open(wikiWordsPath, "r+") as f_in:
     for line in f_in:
         wikiWords.append(line.split(' ')[0])
@@ -23,7 +23,7 @@ with open(wikiWordsPath, "r+") as f_in:
 from ConceptorDebias.Greenwald_1998_Perez_2010_lists import *
 from ConceptorDebias.WEAT_lists import *
 
-winoWordsPath = '/content/' + 'corefBias/WinoBias/wino/extra_gendered_words.txt'
+winoWordsPath = './' + 'corefBias/WinoBias/wino/extra_gendered_words.txt'
 male_vino_extra = []
 female_vino_extra = []
 with open(winoWordsPath, "r+") as f_in:
@@ -31,25 +31,25 @@ with open(winoWordsPath, "r+") as f_in:
         male_vino_extra.append(line.split('\t')[0])
         female_vino_extra.append(line.strip().split('\t')[1])
 
-gnGloveFemaleWordPath = '/content/' + 'gn_glove/wordlist/female_word_file.txt'
+gnGloveFemaleWordPath = './' + 'gn_glove/wordlist/female_word_file.txt'
 female_gnGlove = []
 with open(gnGloveFemaleWordPath, "r+") as f_in:
     for line in f_in:
         female_gnGlove.append(line.strip())
-gnGloveMaleWordPath = '/content/' + 'gn_glove/wordlist/male_word_file.txt'
+gnGloveMaleWordPath = './' + 'gn_glove/wordlist/male_word_file.txt'
 male_gnGlove = []
 with open(gnGloveMaleWordPath, "r+") as f_in:
     for line in f_in:
         male_gnGlove.append(line.strip())
 
-cmuMaleWordPath = '/content/' + 'male.txt'
+cmuMaleWordPath = './' + 'male.txt'
 male_cmu = []
 with open(cmuMaleWordPath, "r+") as f_in:
   for line in f_in:
     w = line.strip()
     if len(w)>0 and w[0] != '#':
       male_cmu.append(w)
-cmuFemaleWordPath = '/content/' + 'female.txt'
+cmuFemaleWordPath = './' + 'female.txt'
 female_cmu = []
 with open(cmuFemaleWordPath, "r+") as f_in:
   for line in f_in:
